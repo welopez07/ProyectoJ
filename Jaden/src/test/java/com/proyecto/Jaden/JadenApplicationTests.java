74e56422c2b5c933d54ee36241fd72aa60e5c07f
wilson <welopez07@gmail.com>
2024-04-05 15:28:16 -0500

    proyecto jaden

---
+++ Jaden/src/test/java/com/proyecto/Jaden/JadenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.Jaden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JadenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
